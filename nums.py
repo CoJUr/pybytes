25b719c08d12297d8adcb71a86b6a105b085fd04
“Cory <coryurton@gmail.com>
2022-09-10 19:34:01 -0500

    completed ch 2, starting ch3: lists

---
+++ nums.py
@@ -0,0 +1,15 @@
+# cory u  9-10-2022
+
+x, y, z = 0, 0, 0
+
+MAX_CONNECTIONS = 500
+
+print(5+3)
+print(10-2)
+print(4*2)
+print(16/2)
+
+favorite_number = 7
+
+print(f"Mi nombre favorito es {favorite_number}")
+
--- simple_message.py
+++ simple_message.py
@@ -1,2 +1,4 @@
+# cory u 9-10-2022
+
 message = "Hello says I"
 print(message)
