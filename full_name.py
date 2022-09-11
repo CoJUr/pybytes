72134896990a79a760845c421e21bbf85fedbf5f
“Cory <coryurton@gmail.com>
2022-09-10 19:02:23 -0500

    whitespace exercises complete, beginning integers

--- full_name.py
+++ full_name.py
@@ -13,3 +13,30 @@ print(message)
 # python 3.5 or earlier uses format() method instead. to use, list the vars wanted in the string inside the parens, referring to each var by a set of ordered braces
 full_name = "{} {}".format(first_name, last_name)
 print(full_name)
+
+# can add a tab to text with \t
+print("Python")
+print("\tPython")
+
+# add a newline in a string with the character combination \n
+print("Languages:\nPython\nC\nJavascript")
+
+# can combine tabs and newlines to organize output.
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+# rstrip() method removes whitespace from the right side of a string, but only temporarily
+favorite_language = 'python   '
+print(favorite_language)
+stripped = favorite_language.rstrip()  # store in a variable to keep the change
+print(stripped)
+
+favorite_language = 'python   '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+# strip from the left with lstrip() or both sides with strip()
+favorite_language = '   python   '
+print(favorite_language)
+both_stripped = favorite_language.strip()
+print(both_stripped)
+print(favorite_language)
---
+++ name_cases.py
@@ -0,0 +1,14 @@
+name = "eric"
+print("Hello " + name + ", would you like to learn to python today?")
+
+print("Lower cased: " + name.lower() + " Upper cased: " + name.upper() + " Title: " + name.title())
+
+famous_person = "Albert Einstein"
+famous_quote = "A person who never made a mistake never tried anything new."
+print(famous_person + " once said, " + famous_quote)
+
+name2 = " \n  \t  Trey \n\t   "
+print(name2)
+print(name2.lstrip())
+print(name2.rstrip())
+print(name2.strip())
